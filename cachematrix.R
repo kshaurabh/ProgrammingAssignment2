678812e3842c23fb37cbe95ac60f6438bc1c3082
kshaurabh <k.shaurabh@gmail.com>
2016-04-23 18:34:57 +0530

    cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,10 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+## Function “makeCacheMatrix” is defined to cache the inverse of a matrix
+## makeCacheMatrix is called to create a matrix; PLZ create only square matrix as solve works on square matrix only
+## the matrix created will be used in cacheSolve function
+## ret_vec is to return the vector x stored in memory.
+## chng_vec is to store the changed vector 
+## we use `<<-` to assign a value to an object in an environment different from the current environment. 
 
 makeCacheMatrix <- function(x = matrix()) {
 	a <- NULL
@@ -18,10 +21,10 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Function “makeCacheMatrix” is defined to cache the inverse of a matrix
-## ret_vec is to return the vector x stored in memory.
-## chng_vec is to store the changed vector
-
+## x: output of makeCacheMatrix()
+## return: inverse of the original matrix input to makeCacheMatrix()
+## if the inverse has already been calculated get it from the cache and skips the computation
+## otherwise, calculates the inverse sets the value of the inverse in the cache via the setinv function.
 cacheSolve <- function(x, ...) {
      a <- x$inverse_2()
       if(!is.null(a)) {
@@ -31,4 +34,5 @@ cacheSolve <- function(x, ...) {
       data <- x$ret_vec()
       a <- solve(data, ...)
       x$inverse_1(a)
-      
+      return(a)
+}    
